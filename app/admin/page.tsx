ff4d12002fdd44608a257297f6395b1276ffcb02
Unique-newbie <uniquepubg3727@gmail.com>
2026-05-21 14:20:05 +0530

    Separate admin dashboard from public header

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,5 @@
-import Link from "next/link";
-import { ArrowRight } from "lucide-react";
 import { AdminDashboard } from "../components/AdminDashboard";
-import { HeaderNav } from "../components/HeaderNav";
-import { NebulintLogo } from "../components/NebulintLogo";
-import { ThemeToggle } from "../components/ThemeToggle";
 
 export default function AdminPage() {
-  return (
-    <>
-      <header className="site-header">
-        <Link href="/" className="brand" aria-label="NEBULINT home">
-          <NebulintLogo />
-        </Link>
-        <HeaderNav />
-        <div className="header-actions">
-          <ThemeToggle />
-          <Link href="/contact" className="nav-cta contact-nav-cta">Contact <ArrowRight size={14} /></Link>
-        </div>
-      </header>
-      <AdminDashboard />
-    </>
-  );
+  return <AdminDashboard />;
 }
